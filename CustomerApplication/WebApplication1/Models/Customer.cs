83fc2ae76cec9b6269042e14006c760fefdcd60a
Kanesh2101 <Jkanesh2101@gmail.com>
2024-08-05 12:29:28 +0800

    added annotations validation to model

--- CustomerApplication/WebApplication1/Models/Customer.cs
+++ CustomerApplication/WebApplication1/Models/Customer.cs
@@ -6,16 +6,26 @@ namespace CustomerApplication.Models
     public class Customer
     {
         [Key]
+        [EmailAddress]
+        [RegularExpression(@"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$")]
+
         public required string Email { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public required string FirstName { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public required string LastName { get; set; }
 
-
+        [RegularExpression(@"\+?6?(?:01[0-46-9]\d{7,8}|0\d{8})")]
+        [DataType(DataType.PhoneNumber)]
         public required string ContactNo { get; set; }
 
-        public string? Address { get; set; }
+        [Required]
+        [StringLength(200)]
+        public required string Address { get; set; }
 
     }
 }
